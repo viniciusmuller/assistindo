e5ebdf33937efc45970b351e6708c6b9d4c88167
Vinícius Müller <vinigm.nho@gmail.com>
2022-12-21 23:05:49 -0300

    feat(api): render work spans in task page

--- frontend/src/TaskPage.tsx
+++ frontend/src/TaskPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import ReactMarkdown from "react-markdown"
 import { useParams } from "react-router-dom"
+import BlankSlate from "./BlankSlate"
 import { Task, trabalhandoService } from "./services/trabalhando-service"
 
 function TaskPage() {
@@ -15,12 +16,13 @@ function TaskPage() {
   return (
     <div className="p-8">
       {task !== null &&
-        <div className="border rounded-lg space-y-2 p-4">
+        <div className="w-full border rounded-lg space-y-2 p-4">
           <h1 className="text-4xl font-bold">{task.name}</h1>
           <div className="space-x-2">
             <label className="text-xl" htmlFor="status-select">
               Status
             </label>
+            {/* TODO: use "value" instead of "defaultValue" */}
             <select name="status-select">
               <option selected={task.status == "todo"}>TODO</option>
               <option selected={task.status == "doing"}>DOING</option>
@@ -28,11 +30,32 @@ function TaskPage() {
             </select>
           </div>
           <hr className="my-2" />
-          <article className="prose">
-            <ReactMarkdown>
-              {task.description}
-            </ReactMarkdown>
-          </article>
+          <div className="flex">
+            <div className="w-1/2 flex flex-col items-start">
+              <article className="prose grow">
+                <ReactMarkdown>
+                  {task.description}
+                </ReactMarkdown>
+              </article>
+              <button>
+                Save
+              </button>
+            </div>
+            <div className="w-1/2">
+              <h2 className="text-2xl font-bold mb-2">Work Spans</h2>
+              <div className="space-y-2">
+                {task.work_spans != null && task.work_spans.length > 0 ? task.work_spans.map(span => (
+                  <div key={span.id} className="p-1 border rounded justify-between">
+                    <div className="flex space-x-4">
+                      <p>Start date: {span.start_date.toLocaleString()}</p>
+                      <p>End date: {span.end_date.toLocaleString()}</p>
+                    </div>
+                    <p>{span.description}</p>
+                  </div>
+                )) : <BlankSlate />}
+              </div>
+            </div>
+          </div>
         </div>
       }
     </div>
